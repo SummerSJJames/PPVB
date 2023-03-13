561444d0248d59c9efee10d83ebe168247dfadb9
Summer <sumxxlove@gmail.com>
2023-03-13 13:33:43 +0100

    Created a sprite and animation for the log

--- PPVB_EndlessRunner/Assets/Scripts/Obstacles/KnockbackTree.cs
+++ PPVB_EndlessRunner/Assets/Scripts/Obstacles/KnockbackTree.cs
@@ -6,14 +6,27 @@ using UnityEngine;
 public class KnockbackTree : MonoBehaviour
 {
     [SerializeField] float knockbackForce;
+    [SerializeField] float speed = 2f;
     Vector3 dir;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce((Vector2.left * speed) - rb.velocity);
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer != 3) return;
         Debug.Log("Knock back Player!");
         dir = (collision.gameObject.transform.position - transform.position).normalized;
-        collision.rigidbody.AddRelativeForce(new Vector2(dir.x * knockbackForce, dir.y * knockbackForce / 5), ForceMode2D.Impulse);
+        collision.rigidbody.AddRelativeForce(new Vector2(dir.x * knockbackForce, dir.y * knockbackForce / 5),
+            ForceMode2D.Impulse);
     }
 
     void OnDrawGizmos()
